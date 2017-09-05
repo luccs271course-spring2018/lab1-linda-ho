0da8d81f60401459982ca2d79794f4718e24096c
Linda Ho <linda.oanh.ho@gmail.com>
2017-09-04 19:56:41 -0500

    First working solution

--- src/main/java/hw/FizzBuzz.java
+++ src/main/java/hw/FizzBuzz.java
@@ -1,4 +1,39 @@
 package hw;
 
 public class FizzBuzz {
+
+    /**
+     * This method prints to the screen numbers starting from one up to the given input choice.
+     * It replaces numbers divisible by 3 with the word 'Fizz'.
+     * It replaces numbers divisible by 5 with the word 'Buzz'.
+     * It replaces numbers divisible by 3 and 5 with the word 'Fizz Buzz'
+     * @param  int  number chosen by a user that determines how many lines to print to the screen.
+     */
+
+    public static void printResult(int numChoice) {
+
+        for(int i = 1; i <= numChoice; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("Fizz Buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
 }
+
+
+/*
+Pseudocode!
+Input: A positive number as a command line argument
+Output: Starting from 1, prints out number to screen based on the following conditions:
+IF number is divisible by 3, print 'Fizz'
+ELSE IF number is divisible by 5, print 'Buzz'
+ELSE IF number is visible by 5 and 3, print 'Fizz Buzz'
+ELSE print number
+Break when number is equal to the input number.
+*/
